fbc347978f0ab5e112c131e1556accabe38415ed
JahsehJ <jahsehjaeger@proton.me>
2023-11-03 23:11:09 -0700

    Remove unused intent

--- bot.py
+++ bot.py
@@ -7,7 +7,6 @@ import config
 class OneMoreRoomBot(commands.Bot):
     def __init__(self):
         intents = discord.Intents.default()
-        intents.message_content = True
         super().__init__(command_prefix=config.prefix, intents=intents)
 
     async def on_ready(self):
