7d2d646e8cda61cfc3a3a01473fee3e36525ec01
JahsehJ <jahsehjaeger@proton.me>
2023-11-03 22:57:31 -0700

    Init commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# One More Room
+
+Simplest solution to messy voice chats
+
+## How it works
+
+If no more empty channel is presented, a new one will be created. (Auto-scaling)  
+This means there will always be 1 extra room for other members!
+
+## Run it
+
+### Requirements
+- python >= 3.11
+- discord.py >= 2.3
+
+```
+# Using a venv is always preferred
+python3 bot.py
+```
+
+## Important notes
+- Stage channels are ignored
+- Manage channel permission is required (Channels that bot has no access to are ignored)
+- Bot has to be up for 24/7, if not, channel duplication is expected
---
+++ one_more_room.py
@@ -0,0 +1,37 @@
+import discord
+from discord.ext import commands
+
+import config
+from bot import OneMoreRoomBot
+
+
+class OneMoreRoom(commands.Cog):
+    def __init__(self, bot: OneMoreRoomBot) -> None:
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_voice_state_update(
+        self,
+        member: discord.Member,
+        before: discord.VoiceState,
+        after: discord.VoiceState,
+    ):
+        if (
+            before.channel
+            and isinstance(before.channel, discord.VoiceChannel)
+            and before.channel.id not in config.ignored_channels
+            and not before.channel.members
+        ):
+            await before.channel.delete()
+
+        if (
+            after.channel
+            and isinstance(after.channel, discord.VoiceChannel)
+            and after.channel.id not in config.ignored_channels
+            and len(after.channel.members) == 1
+        ):
+            await after.channel.clone()
+
+
+async def setup(bot: OneMoreRoomBot):
+    await bot.add_cog(OneMoreRoom(bot))
